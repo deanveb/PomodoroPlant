9d3db5195a7ad7222b02db790697083a6379870a
mxtwng <hoanmytang@gmail.com>
2025-05-06 00:12:06 +0700

    Update _layout.tsx (theme)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme} from '@/hooks/useColorScheme';
+// import TabBarBackground from '@/components/UI/TabBarBackground';
 
 export default function TabLayout() {
+  const color = useColorScheme();
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false}}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: Colors[color ?? 'light'].tint, tabBarInactiveBackgroundColor: Colors[color ?? 'light'].background, tabBarActiveBackgroundColor: Colors[color ?? 'light'].background, headerShown: false}}>
       <Tabs.Screen
         name="Pomodoro"
         options={{
@@ -16,6 +20,12 @@ export default function TabLayout() {
         options={{
           title: 'Garden',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="tree" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="bag"
+        options={{
+          title: 'Bag',
         }}
       />
     </Tabs>
