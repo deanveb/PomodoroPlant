fd72cccee0ee96b2b1da7984aa95be35deaad0cd
deanveb <Annhi1975@gmail.com>
2025-05-17 15:01:24 +0700

    remove login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,74 +1,12 @@
+import { useFocusEffect, useRouter } from "expo-router";
 
-import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
-import { Storage } from 'expo-storage';
-import { useRouter } from "expo-router";
+import { useEffect } from "react";
 
-export default function LoginScreen() {
-  const [name, setName] = useState("");
+import { View, Text } from "react-native";
+
+export default function app() {
   const router = useRouter();
-
-  const handleNext = async () => {
-    if (name.trim()) {
-      try {
-        await Storage.setItem({
-          key: "name",
-          value: name
-        })
-      } catch (error) {
-        console.error("error: ", error);
-      }
-      router.replace('/(tabs)/Pomodoro/pomodoro');
-    } else {
-      alert("Vui lòng nhập tên");
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Chào bạn! Tên bạn là gì?</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nhập tên của bạn"
-        value={name}
-        onChangeText={setName}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleNext}>
-        <Text style={styles.buttonText}>Bắt đầu</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  useFocusEffect(() => {
+    router.replace('/(tabs)/Pomodoro/pomodoro');
+  });
 }
-
-// Style giữ nguyên như đã chỉnh màu xanh ở trên
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20, backgroundColor: "#e6ffe6" }, // nền xanh nhạt
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-    textAlign: "center",
-    color: "#2e7d32", // xanh đậm
-  },
-  input: {
-    height: 40,
-    borderColor: "#4caf50", // xanh lá cây
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-    backgroundColor: "#ffffff",
-  },
-  button: {
-    backgroundColor: "#4caf50", // xanh lá cây
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-    borderColor : "#388e3c", // xanh đậm
-  },
-    buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
